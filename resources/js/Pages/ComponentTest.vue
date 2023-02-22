c3d416316b8e2c65a08cfd52d9b20312c90983bf
Ryo Isogami <isogamiryo6970@gmail.com>
2023-02-22 21:08:04 +0900

    defineEmitsの実装

--- resources/js/Pages/ComponentTest.vue
+++ resources/js/Pages/ComponentTest.vue
@@ -2,11 +2,15 @@
 import GuestLayout from "@/Layouts/GuestLayout.vue";
 import InputLabel from "@/Components/InputLabel.vue";
 import TextInput from "@/Components/TextInput.vue";
+
+const emitTest = e => {
+  console.log(e)
+}
 </script>
 
 <template>
   <GuestLayout>
     <InputLabel value="件名">タイトル</InputLabel>
-    <TextInput modelValue="初期値が入ります"></TextInput>
+    <TextInput modelValue="初期値が入ります" @update:modelValue="emitTest"></TextInput>
   </GuestLayout>
 </template>
