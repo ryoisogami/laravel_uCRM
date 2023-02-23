a0d75c0b16fcac2a2309b720381636d18c679b6c
Ryo Isogami <isogamiryo6970@gmail.com>
2023-02-23 22:08:58 +0900

    44. Item indexの調整・補足

--- resources/js/Pages/Items/index.vue
+++ resources/js/Pages/Items/index.vue
@@ -36,10 +36,13 @@ defineProps({
                             </thead>
                             <tbody>
                               <tr v-for="item in items" :key="item.id">
-                                <td class="px-4 py-3">{{ item.id }}</td>
-                                <td class="px-4 py-3">{{ item.name }}</td>
-                                <td class="px-4 py-3">{{ item.price }}</td>
-                                <td class="px-4 py-3">{{ item.is_selling }}</td>
+                                <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.id }}</td>
+                                <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.name }}</td>
+                                <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.price }}</td>
+                                <td class="border-b-2 border-gray-200 px-4 py-3">
+                                  <span v-if="item.is_selling === 1">販売中</span>
+                                  <span v-if="item.is_selling === 0">停止中</span>
+                                </td>
                               </tr>
                             </tbody>
                           </table>
