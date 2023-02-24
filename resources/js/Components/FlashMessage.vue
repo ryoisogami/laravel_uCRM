fff6058f4a51aeb858b15c9f092c597b5441af98
Ryo Isogami <isogamiryo6970@gmail.com>
2023-02-24 22:09:18 +0900

    50. Items フラッシュメッセージ

--- resources/js/Components/FlashMessage.vue
+++ resources/js/Components/FlashMessage.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div v-if="$page.props.flash.message === 'success'" class="bg-blue-300 text-white p-4">
+  <div v-if="$page.props.flash.status === 'success'" class="bg-blue-300 text-white p-4">
     {{ $page.props.flash.message }}
   </div>
 </template>
