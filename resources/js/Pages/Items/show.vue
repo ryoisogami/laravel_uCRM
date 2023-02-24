eb49b1e6ae314866b95fdabeac7f655a30575d71
Ryo Isogami <isogamiryo6970@gmail.com>
2023-02-24 22:43:36 +0900

    52. JSで bl2br 改行対応

--- resources/js/Pages/Items/show.vue
+++ resources/js/Pages/Items/show.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import { nl2br } from '@/common'
 
 defineProps({
   item: Object
@@ -27,7 +28,7 @@ defineProps({
                               <div class="p-2 w-full">
                                 <div class="relative">
                                   <label for="name" class="leading-7 text-sm text-gray-600">商品名</label>
-                                  <div id="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                  <div id="name" class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                     {{ item.name }}
                                   </div>
                                 </div>
@@ -36,8 +37,8 @@ defineProps({
                               <div class="p-2 w-full">
                                 <div class="relative">
                                   <label for="memo" class="leading-7 text-sm text-gray-600">メモ</label>
-                                  <div id="memo" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">
-                                    {{ item.memo }}
+                                  <div id="memo" v-html="nl2br(item.memo)" class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">
+                                    
                                   </div>
                                 </div>
                               </div>
@@ -45,7 +46,7 @@ defineProps({
                               <div class="p-2 w-full">
                                 <div class="relative">
                                   <label for="price" class="leading-7 text-sm text-gray-600">商品価格</label>
-                                  <div id="price" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                  <div id="price" class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                     {{ item.price }}
                                   </div>
                                 </div>
@@ -54,7 +55,7 @@ defineProps({
                               <div class="p-2 w-full">
                                 <div class="relative">
                                   <label for="status" class="leading-7 text-sm text-gray-600">商品価格</label>
-                                  <div id="status" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                  <div id="status" class="w-full bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                     <span v-if="item.is_selling === 1">販売中</span>
                                     <span v-if="item.is_selling === 0">停止中</span>
                                   </div>
